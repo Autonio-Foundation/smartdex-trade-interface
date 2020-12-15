269a01fdb91f5bf64e238988955f7b3da26af7ee
peter <peter.park13ak@gmail.com>
2020-12-15 03:27:38 -0500

    Change BigNumber format

--- src/util/steps_modals_generation.ts
+++ src/util/steps_modals_generation.ts
@@ -244,7 +244,7 @@ export const getWrapEthStepIfNeeded = (
 
     var needed = amount.multipliedBy(price);
     console.log(needed);
-    const wethAmountNeeded = needed.plus(0.1);
+    const wethAmountNeeded = new BigNumber(needed.plus(0.1).toString());
     console.log(wethAmountNeeded);
 
     // If we have enough WETH, we don't need to wrap
