a4737396db4595d6d9fce4e0afe1f4cbaf04d04a
peter <peter.park13ak@gmail.com>
2020-12-15 02:07:56 -0500

    Fix add function

--- src/util/steps_modals_generation.ts
+++ src/util/steps_modals_generation.ts
@@ -241,8 +241,8 @@ export const getWrapEthStepIfNeeded = (
         return null;
     }
 
-    const wethAmountNeeded = amount.multipliedBy(price) + 0.0001;
-
+    const wethAmountNeeded = amount.multipliedBy(price).add(0.0001);
+    
     // If we have enough WETH, we don't need to wrap
     if (wethTokenBalance.balance.isGreaterThan(wethAmountNeeded)) {
         return null;
