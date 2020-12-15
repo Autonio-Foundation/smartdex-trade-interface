0a660f82559ad7b39a6c8304a867caf3c3224f86
peter <peter.park13ak@gmail.com>
2020-12-15 03:09:42 -0500

    Plus 0.1 to wMatic when trading.

--- src/util/steps_modals_generation.ts
+++ src/util/steps_modals_generation.ts
@@ -242,7 +242,7 @@ export const getWrapEthStepIfNeeded = (
         return null;
     }
 
-    const wethAmountNeeded = amount.multipliedBy(price);
+    const wethAmountNeeded = amount.multipliedBy(price).plus(0.1);
 
     // If we have enough WETH, we don't need to wrap
     if (wethTokenBalance.balance.isGreaterThan(wethAmountNeeded)) {
