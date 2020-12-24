dd647ea92ced3bc20bb3537782153566ae093b60
peter <peter.park13ak@gmail.com>
2020-12-24 02:43:12 -0500

    fix websocket endpoint

--- src/components/erc20/marketplace/TVChartContainer/api/streaming.js
+++ src/components/erc20/marketplace/TVChartContainer/api/streaming.js
@@ -1,6 +1,6 @@
 import { parseFullSymbol } from './helpers.js';
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "wss://streamer.cryptocompare.com";
+const ENDPOINT = "ws://18.188.255.60";
 
 export const socket = socketIOClient(ENDPOINT);
 // const socket = io('wss://streamer.cryptocompare.com');
