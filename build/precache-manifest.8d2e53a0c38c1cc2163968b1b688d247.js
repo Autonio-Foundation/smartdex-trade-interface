75a8dc740fa92f1a4cd3c638ee9ee412689efd60
root <root@ip-172-31-30-185.us-east-2.compute.internal>
2021-01-05 16:53:14 +0000

    build

---
+++ build/precache-manifest.8d2e53a0c38c1cc2163968b1b688d247.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6dc40b343986339eb8669d5d791f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16b896a80276d53772f3",
+    "url": "/static/css/2.97edab10.chunk.css"
+  },
+  {
+    "revision": "b2f42235ac457d3a18d2",
+    "url": "/static/css/main.f80846ed.chunk.css"
+  },
+  {
+    "revision": "16b896a80276d53772f3",
+    "url": "/static/js/2.69491653.chunk.js"
+  },
+  {
+    "revision": "b2f42235ac457d3a18d2",
+    "url": "/static/js/main.5eecc37a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7e0e1916fae503eb0b98d66f31fc0dd",
+    "url": "/static/media/erc20_logo.a7e0e191.svg"
+  },
+  {
+    "revision": "c85884b42dd0fa75dfcb0b24bdd1745b",
+    "url": "/static/media/erc721_logo.c85884b4.svg"
+  },
+  {
+    "revision": "6d3ab03c52315cca1e95b8e2e6489426",
+    "url": "/static/media/install_metamask.6d3ab03c.svg"
+  },
+  {
+    "revision": "bac7491467df990edc64591ffc639d62",
+    "url": "/static/media/red_exclamation_sign.bac74914.svg"
+  }
+]);
