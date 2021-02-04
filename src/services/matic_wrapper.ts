c0033c06a7e14d17aaeae16c72f65aaeb5111175
peter <peter.park13ak@gmail.com>
2021-02-04 16:24:03 -0500

    fix

--- src/services/matic_wrapper.ts
+++ src/services/matic_wrapper.ts
@@ -1,18 +1,20 @@
-import Matic from '@maticnetwork/maticjs';
+import { MaticPOSClient } from '@maticnetwork/maticjs';
 import { INFURA_PROVIDER } from '../common/constants';
 
 import { sleep } from '../util/sleep';
 
-let maticWrapper: Matic.MaticPOSClient | null = null;
+// const MaticPOSClient = require('@maticnetwork/maticjs').MaticPOSClient
 
-export const initializeMaticWrapper = async (): Promise<Matic.MaticPOSClient | null> => {
+let maticWrapper: MaticPOSClient | null = null;
+
+export const initializeMaticWrapper = async (): Promise<MaticPOSClient | null> => {
     const { ethereum, web3 } = window;
 
     if (maticWrapper) {
         return maticWrapper;
     }
 
-    maticWrapper = new Matic.MaticPOSClient({
+    maticWrapper = new MaticPOSClient({
         network: 'mainnet',
         version: 'v1',
         maticProvider: ethereum ? ethereum : web3.currentProvider,
@@ -24,7 +26,7 @@ export const initializeMaticWrapper = async (): Promise<Matic.MaticPOSClient | n
     return maticWrapper;
 };
 
-export const getWeb3Wrapper = async (): Promise<Matic.MaticPOSClient> => {
+export const getWeb3Wrapper = async (): Promise<MaticPOSClient> => {
     while (!maticWrapper) {
         // if web3Wrapper is not set yet, wait and retry
         await sleep(100);
