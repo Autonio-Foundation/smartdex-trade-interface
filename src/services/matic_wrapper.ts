c94f8f08053419f6253f270e189423282106d1ac
peter <peter.park13ak@gmail.com>
2021-02-05 13:30:34 -0500

    try

--- src/services/matic_wrapper.ts
+++ src/services/matic_wrapper.ts
@@ -1,6 +1,5 @@
 import Matic from '@maticnetwork/maticjs';
 import { MaticPOSClient } from '@maticnetwork/maticjs';
-import MetamaskProvider from "@maticnetwork/metamask-provider"
 import { MATIC_PROVIDER, INFURA_PROVIDER } from '../common/constants';
 
 import { sleep } from '../util/sleep';
@@ -17,19 +16,11 @@ export const initializeMaticWrapper = async (): Promise<Matic | null> => {
         return maticWrapper;
     }
 
-    const ethereumProvider = new MetamaskProvider(ethereum, {
-        url: INFURA_PROVIDER
-    })
-
-    const maticProvider = new MetamaskProvider(ethereum, {
-        url: MATIC_PROVIDER
-    })
-
     maticWrapper = new Matic({
         network: 'mainnet',
         version: 'v1',
-        maticProvider: maticProvider,
-        parentProvider: ethereumProvider
+        maticProvider: MATIC_PROVIDER,
+        parentProvider: INFURA_PROVIDER
     });
 
     maticWrapper.initialize();
@@ -37,8 +28,8 @@ export const initializeMaticWrapper = async (): Promise<Matic | null> => {
     maticPoSClient = new MaticPOSClient({
         network: 'mainnet',
         version: 'v1',
-        maticProvider: maticProvider,
-        parentProvider: ethereumProvider
+        maticProvider: MATIC_PROVIDER,
+        parentProvider: INFURA_PROVIDER
     });
 
     return maticWrapper;
