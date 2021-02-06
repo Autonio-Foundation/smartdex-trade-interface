3f437ffd95242f0799d9161463d79f29943d6240
root <root@ip-172-31-30-185.us-east-2.compute.internal>
2021-02-06 19:54:05 +0000

    build

---
+++ build/precache-manifest.1b738fad96c136173d81b966f0915333.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc9f095b87ced8eaa9572264210209f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bfa770332a456878db3",
+    "url": "/static/css/2.97edab10.chunk.css"
+  },
+  {
+    "revision": "d139b8a5a6581dcb0957",
+    "url": "/static/css/main.f6fd69c2.chunk.css"
+  },
+  {
+    "revision": "3bfa770332a456878db3",
+    "url": "/static/js/2.dd15aaac.chunk.js"
+  },
+  {
+    "revision": "d139b8a5a6581dcb0957",
+    "url": "/static/js/main.1b6df266.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7e0e1916fae503eb0b98d66f31fc0dd",
+    "url": "/static/media/erc20_logo.a7e0e191.svg"
+  },
+  {
+    "revision": "c85884b42dd0fa75dfcb0b24bdd1745b",
+    "url": "/static/media/erc721_logo.c85884b4.svg"
+  },
+  {
+    "revision": "6d3ab03c52315cca1e95b8e2e6489426",
+    "url": "/static/media/install_metamask.6d3ab03c.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13f6d28fb65633388c4776bed0998729.js"
+  "/precache-manifest.1b738fad96c136173d81b966f0915333.js"
 );
 
 self.addEventListener('message', (event) => {
