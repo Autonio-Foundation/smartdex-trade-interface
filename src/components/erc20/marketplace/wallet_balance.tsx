c7449a4970dcdbe132a62237686c8f54c0f99806
peter <peter.park13ak@gmail.com>
2021-02-18 03:24:16 -0500

    update balance details

--- src/components/erc20/marketplace/wallet_balance.tsx
+++ src/components/erc20/marketplace/wallet_balance.tsx
@@ -1,4 +1,5 @@
 import { BigNumber, OrderStatus } from '0x.js';
+import { Web3Wrapper } from '@0x/web3-wrapper';
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
@@ -218,6 +219,7 @@ class WalletBalance extends React.Component<Props, State> {
             currencyPair,
             onConnectWallet,
             baseToken,
+            quoteToken,
             quoteTokenBalance,
             baseTokenBalance,
             totalEthBalance,
@@ -234,7 +236,10 @@ class WalletBalance extends React.Component<Props, State> {
                         baseTokenBalanceAmount = baseTokenBalanceAmount.minus(cur.size);
                     }
                     else {
-                        quoteTokenBalanceAmount = quoteTokenBalanceAmount.minus(cur.size.multipliedBy(cur.price));
+                        const priceInQuoteBaseUnits = Web3Wrapper.toBaseUnitAmount(cur.price, quoteToken.decimals);
+                        const baseTokenAmountInUnits = Web3Wrapper.toUnitAmount(cur.size, baseToken.decimals);
+            
+                        quoteTokenBalanceAmount = quoteTokenBalanceAmount.minus(baseTokenAmountInUnits.multipliedBy(priceInQuoteBaseUnits));
                     }    
                 }
             })
