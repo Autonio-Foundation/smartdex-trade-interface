69a6acfb77b1f6af30afe332b56375b2cd396481
peter <peter.park13ak@gmail.com>
2021-03-05 15:46:45 -0500

    remove re-rendering

--- src/store/relayer/actions.ts
+++ src/store/relayer/actions.ts
@@ -61,8 +61,6 @@ export const getAllOrders: ThunkCreator = () => {
         const makerAddresses = getMakerAddresses(state);
         try {
             let uiOrders: UIOrder[] = [];
-            dispatch(setOrders([]));
-            dispatch(setOverallHistory([]));
             const isWeb3NotDoneState = [Web3State.Locked, Web3State.NotInstalled, Web3State.Error].includes(web3State);
             // tslint:disable-next-line:prefer-conditional-expression
             if (isWeb3NotDoneState) {
