7f2bfcde7c6e7d479646c583bb48f3d7f099bebc
daiki saito <daiki.saito92@gmail.com>
2021-03-19 04:17:52 +0900

    add random route handling

--- src/index.tsx
+++ src/index.tsx
@@ -29,12 +29,14 @@ const Web3WrappedApp = (
             <AppContainer>
                 <Switch>
                     <Route
+                        exact
                         path="/"
                         component={MaintenancePage}
                     />
                     {/* <Route path={ERC20_APP_BASE_PATH} component={Erc20App} />
                     <Route path={ERC721_APP_BASE_PATH} component={Erc721App} />
                     <Route component={RedirectToHome} /> */}
+                    <Route render={() => <Redirect to="/" />} />
                 </Switch>
             </AppContainer>
         </ConnectedRouter>
